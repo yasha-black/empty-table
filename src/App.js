947e8c2eadf444dd2f1ace0c9b47b54f12b2b7df
yashablack <iamyashablack@gmail.com>
2021-10-07 21:01:30 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'antd/dist/antd.css'
+import { Table, Button, Typography } from 'antd'
+
+// Table Component usage
+// https://ant.design/components/table/
+
+// Button Component usage
+// https://ant.design/components/button/
+
+const dataSource = [
+    {
+        key: '1',
+        name: 'Mike',
+        age: 32,
+        address: '10 Downing Street',
+    },
+    {
+        key: '2',
+        name: 'John',
+        age: 42,
+        address: '10 Downing Street',
+    },
+];
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
 
 function App() {
+  const handleClick = () => {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App__container">
+            <Typography.Title>Title</Typography.Title>
+            <Table
+                style={{marginBottom: '1em'}}
+                dataSource={dataSource}
+                columns={columns}
+                pagination={false}
+            />
+            <Button onClick={handleClick} type="primary">
+                Click me
+            </Button>
+        </div>
     </div>
   );
 }
